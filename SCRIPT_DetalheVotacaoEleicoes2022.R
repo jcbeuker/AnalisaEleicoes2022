94d46a7012bf57703f3992a18b729016ec6ab09a
jcbeuker <jcbeuker@yahoo.com.br>
2022-12-18 11:02:12 -0300

    Importando mais pacotes e documentando

--- SCRIPT_DetalheVotacaoEleicoes2022.R
+++ SCRIPT_DetalheVotacaoEleicoes2022.R
@@ -1,13 +1,25 @@
 ################################################################################
-# Análise dos detalhe de votação nas eleições 2022
+# Análise dos detalhe de votação nas eleições 2022 (Fatorial PCA)
 # Dados obtidos em:
 # https://sig.tse.jus.br/ords/dwapr/seai/r/sig-eleicao/estatisticas-eleicao?session=202624093615400
 # José Caetano Beuker
 # 17.12.2022
 ################################################################################
 # Pacotes necessários
-pacotes <- c("tidyverse","ggrepel","reshape2","knitr","kableExtra",
-             "PerformanceAnalytics","factoextra","psych","tigris","dplyr")
+pacotes <- c("plotly", # plataforma gráfica
+             "tidyverse", # carregar outros pacotes do R
+             "ggrepel", # geoms de texto e rótulo para 'ggplot2' que ajudam a
+             # evitar sobreposição de textos
+             "reshape2",# função 'melt'
+             "knitr", "kableExtra", # formatação de tabelas
+             "PerformanceAnalytics", # função 'chart.Correlation' para plotagem
+             "factoextra", # facilita a extração e visualização de dados multivalorados
+             "psych", # elaboração da fatorial e estatísticas
+             "ltm", # determinação do alpha de Cronbach pela função 'cronbach.alpha'
+             "Hmisc", # matriz de correlações com p-valor
+             "tigris", # baixar e usar arquivos formatados de fronteiras cartográficas
+             "dplyr" # Realizar a transformação de dados
+             )
 
 if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
   instalador <- pacotes[!pacotes %in% installed.packages()]
@@ -30,5 +42,11 @@ View(detalhe_votacao)
 # Objeto detalhes de MG
 DetalhaMG <- detalhe_votacao[detalhe_votacao$sg_uf == "MG",]
 
-# Resumo de detalhes de MG
-summary(DetalhaMG)
+# Estatísticas descritivas das variaveis
+summary(DetalhaMG[,7:17])
+
+#Coeficientes de correlação de Pearson para cada par de variáveis
+rho <- rcorr(as.matrix(DetalhaMG[,7:17]), type="pearson")
+
+corr_coef <- rho$r # matriz de correlações
+corr_sig <- round(rho$P, 5) # matriz com p-valor dos coeficientes
